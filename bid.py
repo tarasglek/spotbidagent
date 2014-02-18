411e4c87ae2224360659671541e38ecc69a356f0
Rail Aliiev <rail@mozilla.com>
2014-02-18 15:00:51 -0500

    Simplify the logic

--- bid.py
+++ bid.py
@@ -1,12 +1,36 @@
 import boto.ec2
 import pickle
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def get_spot_price_history(region):
+        r = boto.ec2.get_region(region)
+        # TODO: use secrets
+        ec2 = r.connect()
+        return ec2.get_spot_price_history()
+
+
+def get_current_spot_prices(region):
+    all_prices = get_spot_price_history(region)
+    # make sure to sort them by the timestamp, so we don't process the same
+    # entry twice
+    all_prices = sorted(all_prices, key=lambda x: x.timestamp, reverse=True)
+    current_prices = {}
+    for price in all_prices:
+        az = price.availability_zone
+        instance_type = price.instance_type
+        if not current_prices.get(instance_type):
+            current_prices[instance_type] = {}
+        if not current_prices[instance_type].get(az):
+            current_prices[instance_type][az] = price.price
+    return current_prices
 
 
 def download_pricelist(region):
     with open("price.%s.pickle" % (region), "w") as out:
-        r = boto.ec2.get_region(region)
-        ec2 = r.connect()
-        history = ec2.get_spot_price_history()
+        history = get_spot_price_history(region)
         pickle.dump(history, out)
     return history
 
@@ -22,21 +46,24 @@ def get_pricelist_disk(region):
 
 
 class Spot:
-    def __init__(self, spotEntry, max_price, performance_constant):
-        self.spotEntry = spotEntry
-        self.max_price = max_price
+    def __init__(self, instance_type, availability_zone, current_price,
+                 bid_price, performance_constant):
+        self.instance_type = instance_type
+        self.availability_zone = availability_zone
+        self.current_price = current_price
+        self.bid_price = bid_price
         self.performance_constant = performance_constant
 
     def __repr__(self):
-        return "%s(%s) %g(%g) < %g" % (
-            self.spotEntry.instance_type, self.spotEntry.availability_zone,
-            self.spotEntry.price, self.value(), self.max_price)
+        return "<%s (%s) %g (value: %g) < %g" % (
+            self.instance_type, self.availability_zone,
+            self.current_price, self.value(), self.bid_price)
 
     def __str__(self):
         return self.__repr__()
 
     def value(self):
-        return self.spotEntry.price / self.performance_constant
+        return self.current_price / self.performance_constant
 
     def __lt__(self, other):
         return self.value() < other.value()
@@ -44,41 +71,24 @@ class Spot:
 
 def decide(rules, regions):
     choices = []
-    regions = map(get_pricelist_disk, regions)
-    for r in rules:
-        if type(r[2]) == str:
-            continue
-        spot_azs = {}
-        for history in regions:
-            for h in history:
-                if h.instance_type != r[0]:
-                    continue
-                if not h.availability_zone in spot_azs:
-                    spot_azs[h.availability_zone] = h
-                    continue
-                s = spot_azs[h.availability_zone]
-                if s.timestamp <= h.timestamp:
-                    spot_azs[h.availability_zone] = h
-                    print "this never happens because boto sorts these"
-            if len(spot_azs) == 0:
-                print "No spot info for %s" % r[0]
-                continue
-
-            for az, spot in spot_azs.iteritems():
-                if spot.price < r[1]:
-                    choices.append(Spot(spot, r[1], r[2]))
-                else:
-                    print "%s(in %s) too expensive as of %s" % (
-                        spot, spot.availability_zone, spot.timestamp)
-            print spot_azs.keys()
-    print "\n".join(map(str, sorted(choices)))
-            #print history
+    prices = {}
+    for region in regions:
+        prices.update(get_current_spot_prices(region))
+    for rule in rules:
+        instance_type, bid_price, perf_const = rule
+        for az, price in prices.get(instance_type, {}).iteritems():
+            if price > bid_price:
+                log.debug("%s (in %s) too expensive", price, az)
+            else:
+                choices.append(Spot(instance_type, az, price,
+                                    bid_price, perf_const))
+    choices.sort()
+    return choices
 
 if __name__ == "__main__":
     ret = decide(
         [["c3.xlarge", 0.250, 1],
          ["m3.xlarge", 0.250, 1.1],
-         ["m3.large", 0.150, 0.6],
-         ["c3.2xlarge", 0.250, 1.2],
-         ["c3.xlarge", 0.300, "ondemand"]],
-        ['us-west-2', 'us-east-1', 'us-west-1', 'eu-west-1'])
+         ["c3.2xlarge", 0.250, 1.2]],
+        ['us-west-2', 'us-east-1'])
+    print "\n".join(map(str, ret))
