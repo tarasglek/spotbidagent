86398834d0ae82a3f27d5de3b9db71d5119dee5a
Rail Aliiev <rail@mozilla.com>
2014-02-18 17:37:10 -0500

    Add region information

--- bid.py
+++ bid.py
@@ -9,6 +9,7 @@ def get_current_spot_prices(connection):
     # make sure to sort them by the timestamp, so we don't process the same
     # entry twice
     all_prices = sorted(all_prices, key=lambda x: x.timestamp, reverse=True)
+    region = connection.region.name
     current_prices = {}
     for price in all_prices:
         az = price.availability_zone
@@ -17,21 +18,22 @@ def get_current_spot_prices(connection):
             current_prices[instance_type] = {}
         if not current_prices[instance_type].get(az):
             current_prices[instance_type][az] = price.price
-    return current_prices
+    return {region: current_prices}
 
 
 class Spot:
-    def __init__(self, instance_type, availability_zone, current_price,
+    def __init__(self, instance_type, region, availability_zone, current_price,
                  bid_price, performance_constant):
         self.instance_type = instance_type
+        self.region = region
         self.availability_zone = availability_zone
         self.current_price = current_price
         self.bid_price = bid_price
         self.performance_constant = performance_constant
 
     def __repr__(self):
-        return "%s (%s) %g (value: %g) < %g" % (
-            self.instance_type, self.availability_zone,
+        return "%s (%s, %s) %g (value: %g) < %g" % (
+            self.instance_type, self.region, self.availability_zone,
             self.current_price, self.value(), self.bid_price)
 
     def __str__(self):
@@ -53,12 +55,16 @@ def decide(connections, rules):
         instance_type = rule["instance_type"]
         bid_price = rule["bid_price"]
         performance_constant = rule["performance_constant"]
-        for az, price in prices.get(instance_type, {}).iteritems():
-            if price > bid_price:
-                log.debug("%s (in %s) too expensive", price, az)
-            else:
-                choices.append(Spot(instance_type, az, price,
-                                    bid_price, performance_constant))
+        for region, region_prices in prices.iteritems():
+            for az, price in region_prices.get(instance_type, {}).iteritems():
+                if price > bid_price:
+                    log.debug("%s (in %s) too expensive", price, az)
+                else:
+                    choices.append(
+                        Spot(instance_type=instance_type, region=region,
+                             availability_zone=az, current_price=price,
+                             bid_price=bid_price,
+                             performance_constant=performance_constant))
     # sort by self.value()
     choices.sort()
     return choices
