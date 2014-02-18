e7a807161ebc2dd0b64e8197e7d83ac462998fcd
Taras Glek <github@glek.net>
2014-02-18 10:44:23 -0800

    indentation bug

--- bid.py
+++ bid.py
@@ -36,8 +36,18 @@ class Spot:
         return self.value() < other.value()
 
 def decide(rules, regions):
+    def sort_newest2oldest(ls):
+        def cmp(a, b):
+            if b.timestamp == a.timestamp:
+                return 0
+            elif b.timestamp > a.timestamp:
+                return 1
+            else:
+                return -1
+        return sorted(ls, cmp)
     choices = []
     regions = map(get_pricelist_disk, regions)
+    regions = map(sort_newest2oldest, regions)
     for r in rules:
         if type(r[2]) == str:
             continue
@@ -48,24 +58,21 @@ def decide(rules, regions):
                     continue
                 if not h.availability_zone in spot_azs:
                     spot_azs[h.availability_zone] = h
-                    continue
-                s = spot_azs[h.availability_zone]
-                if s.timestamp <= h.timestamp:
-                    spot_azs[h.availability_zone] = h
-                    print "this never happens because boto sorts these"
+           
             if len(spot_azs) == 0:
                 print "No spot info for %s" % r[0]
                 continue
 
-            for az, spot in spot_azs.iteritems():
-                if spot.price < r[1]:
-                    choices.append(Spot(spot, r[1], r[2]))
-                else:
-                    print "%s(in %s) too expensive as of %s" % (spot, spot.availability_zone, spot.timestamp)
-            print spot_azs.keys()
-    print "\n".join(map(str, sorted(choices)))
-            #print history
+        for az, spot in spot_azs.iteritems():
+            if spot.price < r[1]:
+                s = Spot(spot, r[1], r[2])
+                choices.append(s)
+            else:
+                #print "%s(in %s) too expensive as of %s" % (spot, spot.availability_zone, spot.timestamp)
+                pass
+    return sorted(choices)
 
 if __name__ == "__main__":
     ret = decide([["c3.xlarge", 0.250, 1], ["m3.xlarge", 0.250, 1.1], ["m3.large", 0.150, 0.6], ["c3.2xlarge", 0.250, 1.2], ["c3.xlarge", 0.300, "ondemand"]], ['us-west-2','us-east-1', 'us-west-1', 'eu-west-1'])
+    print "\n".join(map(str, ret))
     
